3624fff8da914fc158985c80f319e8be93b50d18
student <rod.qasolutions@gmail.com>
2021-04-15 00:43:36 +0000

    photshop respository

---
+++ src/main/java/photoshop2/demo/Ph2Application.java
@@ -0,0 +1,13 @@
+package photoshop2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ph2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ph2Application.class, args);
+	}
+
+}
---
+++ src/test/java/photoshop2/demo/Ph2ApplicationTests.java
@@ -0,0 +1,13 @@
+package photoshop2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ph2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
